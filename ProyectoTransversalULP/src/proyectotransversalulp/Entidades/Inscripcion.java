0913e4d1a7439243ad6183bfa7c983295b0daba9
MoonshineJules <moonshineshules@gmail.com>
2023-09-08 12:38:02 -0300

    Clases Entidades 2

--- ProyectoTransversalULP/src/proyectotransversalulp/Entidades/Inscripcion.java
+++ ProyectoTransversalULP/src/proyectotransversalulp/Entidades/Inscripcion.java
@@ -11,4 +11,59 @@ package proyectotransversalulp.Entidades;
  */
 public class Inscripcion {
     
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private Double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, Double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, Double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
 }
--- ProyectoTransversalULP/src/proyectotransversalulp/Entidades/Materia.java
+++ ProyectoTransversalULP/src/proyectotransversalulp/Entidades/Materia.java
@@ -11,4 +11,64 @@ package proyectotransversalulp.Entidades;
  */
 public class Materia {
     
+    private int idMateria;
+    private String nombre;
+    private int añoMateria;
+    private Boolean activo;
+
+    public Materia() {
+        
+    }
+
+    public Materia(int idMateria, String nombre, int añoMateria, Boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.añoMateria = añoMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int añoMateria, Boolean activo) {
+        this.nombre = nombre;
+        this.añoMateria = añoMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAñoMateria() {
+        return añoMateria;
+    }
+
+    public void setAñoMateria(int añoMateria) {
+        this.añoMateria = añoMateria;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", a\u00f1oMateria=" + añoMateria + ", activo=" + activo + '}';
+    }
+    
+    
 }
